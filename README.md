bd575660b314d3a78afe494cf83b4e3f2b86b922
Hrvoje Ditrih <hrvoje.ditrih@gmail.com>
2023-05-16 18:01:07 +0200

    chrome extension

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # quickstart-web-npm-js
 
-- Go to https://developer.deepar.ai
-  - Sign up
-  - Create a project and a Web app
-  - Copy the license key
-  - Paste it into `src/index.js` (replace `your_license_key_goes_here`)
-- Open the terminal in the root of the project
-  - Run `npm install`
-  - Run `npm run dev`
-  - If the browser doesn't open automatically, open http://localhost:8888
+## Using DeepAR in Chrome extension
+
+Make sure to use the [deepar](https://www.npmjs.com/package/deepar) npm package with a `chromeExtension` version.
+For example `deepar@5.2.0-chromeExtension`.
+
+To use DeepAR inside Chrome extension add the following CSP to manifest.
+```json lines
+"content_security_policy": {
+  "extension_pages": "script-src 'self' 'wasm-unsafe-eval'; object-src 'self';"
+}
+```
+It is important to have `wasm-unsafe-eval` CSP flag otherwise Chrome extension cannot execute wasm code that is needed to run DeepAR.
+
+> ⚠️ DeepAR cannot run inside Sandbox pages.
+
+
+- Contact DeepAR Support to setup dev and prod license keys for Chrome extension.
+- Paste license key into `src/index.js` (replace `your_license_key_goes_here`).
+- Run `npm install`.
+- Run `npm run dev`.
+- The `dist` folder now contains the Chrome extension packet.
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import * as deepar from 'deepar';
 // Log the version. Just in case.
 console.log("Deepar version: " + deepar.version);
 
+const btnStart = document.getElementById("btnStart");
+const loadWrapper = document.getElementById("loader-wrapper");
+
+btnStart.onclick = async () => {
+  btnStart.style.display = "none";
+  loadWrapper.style.display = "block";
+  await startFn();
+}
+
 // Top-level await is not supported.
 // So we wrap the whole code in an async function that is called immediatly.
-(async function() {
+const startFn = async function() {
 
   // Resize the canvas according to screen size.
   const canvas = document.getElementById('deepar-canvas');
@@ -31,10 +40,10 @@ console.log("Deepar version: " + deepar.version);
     'effects/Fire_Effect.deepar',
     'effects/Elephant_Trunk.deepar'
   ];
-  
+
   // Initialize DeepAR with an effect file.
   const deepAR = await deepar.initialize({
-    licenseKey: 'your_license_key_goes_here',
+    licenseKey: '88228022645b747cdf497fa69d4ee9caa4ced3b31acb305fe10a2a7cc692dc30975e51a7b0109dff',
     canvas: canvas,
     rootPath: "./deepar-resources", // See webpack.config.js and package.json build script.
     effect: effectList[0]
@@ -42,7 +51,7 @@ console.log("Deepar version: " + deepar.version);
 
   // Hide the loading screen.
   document.getElementById("loader-wrapper").style.display = "none";
-  
+
   // Position the carousel to cover the canvas
   if (window.innerWidth > window.innerHeight) {
     const width = Math.floor(window.innerHeight * 0.66);
@@ -50,7 +59,7 @@ console.log("Deepar version: " + deepar.version);
     carousel.style.width = width + 'px';
     carousel.style.marginLeft = (window.innerWidth - width) / 2 + "px";
   }
-  
+
   // Configure carousel.
   $(document).ready(() => {
     $('.effect-carousel').slick({
@@ -61,7 +70,7 @@ console.log("Deepar version: " + deepar.version);
       accessibility: false,
       variableWidth: true,
     });
-  
+
     // Switch the effect when carusel moves.
     $('.effect-carousel').on('afterChange', async function (event, slick, currentSlide) {
       await deepAR.switchEffect(effectList[currentSlide]);
@@ -70,4 +79,4 @@ console.log("Deepar version: " + deepar.version);
 
   });
 
-})();
+};
